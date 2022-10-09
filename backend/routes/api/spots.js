169ee8d0ab9f091377d029c2c6f9b7aec190484b
jack <jzhang319@gmail.com>
2022-10-09 17:05:55 -0400

    finished migration, models, and associations

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const { Spot, spotImage } = require("../../db/models");
+const { User } = require("../../db/models");
+const { requireAuth, restoreUser } = require("../../utils/auth.js");
 const router = express.Router();
 
-//! GET all spots for testing
+// GET current user
+router.get("/current", restoreUser, async (req, res) => {
+  const { user } = req;
+  // console.log(user.id, `-----------------------`);
+  const currUser = await User.findByPk(user.id);
+  // console.log(currUser, ` -------------------`);
+  res.json(currUser);
+});
+
+// GET all spots
 router.get("/", async (req, res) => {
   const allSpots = await Spot.findAll({ include: spotImage });
 
