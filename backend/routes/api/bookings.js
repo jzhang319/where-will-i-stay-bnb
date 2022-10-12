37f3b8ec4839c94ca119dc52c57d668458411488
jzhang319 <jzhang319@gmail.com>
2022-10-12 13:53:10 -0400

    working on current user booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -5,15 +5,36 @@ const {
   User,
   Review,
   ReviewImage,
+  Booking,
 } = require("../../db/models");
 const { requireAuth, restoreUser } = require("../../utils/auth.js");
 const router = express.Router();
 
-
-
-
-
-
-
+// GET all of current User's bookings
+router.get("/current", requireAuth, async (req, res) => {
+  const { user } = req;
+  const currBookings = await Booking.findAll({
+    where: { userId: user.id },
+    include: [
+      {
+        model: Spot,
+        include: [
+          {
+            model: SpotImage,
+            where: { preview: true },
+            attribute: ["url"],
+          },
+        ],
+      },
+    ],
+  });
+  // console.log(currBookings);
+  let bookingArray = [];
+  currBookings.forEach((booking) => {
+    bookingArray.push(booking.toJSON());
+  });
+  console.log(bookingArray);
+  res.json(bookingArray);
+});
 
 module.exports = router;
