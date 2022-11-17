677e51c1fb59662d24a0d951049e962a5458c8d9
jack <jzhang319@gmail.com>
2022-11-16 20:37:51 -0500

    finish phase 0

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,20 +3,52 @@ const router = express.Router();
 
 // // testing route only
 // router.get('/hello/world', function(req, res) {
-  //   res.cookie('XSRF-TOKEN', req.csrfToken());
-  //   res.send('Hello World!');
-  // });
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
-  router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      "XSRF-TOKEN": csrfToken,
-    });
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-TOKEN": csrfToken,
+  });
+});
+
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
+
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  // Serve the frontend's index.html file at the root route
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
   });
 
-  const apiRouter = require("./api");
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
 
-  router.use("/api", apiRouter);
-  
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
 module.exports = router;
