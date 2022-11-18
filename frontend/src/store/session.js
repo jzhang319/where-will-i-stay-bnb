aa0d3b22f7537d1179aebba1c3350082960333dc
jack <jzhang319@gmail.com>
2022-11-17 20:18:36 -0500

    finished setting form

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -29,6 +29,13 @@ export const login = (user) => async (dispatch) => {
   }
 };
 
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
+
 const initialState = {
   user: null,
 };
