3ff6a6aaf72c748f64270719ab2cb33a7d4950f7
jzhang319 <jzhang319@gmail.com>
2022-12-06 10:09:14 -0500

    updated

--- frontend/src/components/CurrentUserBooking/CurrentUserBooking.js
+++ frontend/src/components/CurrentUserBooking/CurrentUserBooking.js
@@ -1,7 +1,27 @@
 import "./CurrentUserBooking.css";
+import { getAllBookings } from "../../store/booking";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 
 const CurrentUserBooking = () => {
+  const { SpotId } = useParams();
+  const dispatch = useDispatch();
+
+  const spot = useSelector((state) => state.spot)
+  console.log(spot, ` <-- currentUserBooking`)
+
+  useEffect(()=>{
+    dispatch(getAllBookings(SpotId));
+  },[])
+
+  useEffect(() => {
+    getAllBookings()
+     .then((res) => {
+        setBookings(res);
+      })
+
   return (
     <div className="current-user-booking-container">
       <div>Current User Booking(s):</div>
