d078367be2ec7222038ac9a2c8732c7e4759f103
jack <jzhang319@gmail.com>
2022-12-08 19:24:13 -0500

    signup working now

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -23,7 +23,13 @@ function SignupFormPage() {
     if (password === confirmPassword) {
       setErrors([]);
       return dispatch(
-        sessionActions.signup({ email, username, password }),
+        sessionActions.signup({
+          firstName,
+          lastName,
+          email,
+          username,
+          password,
+        }),
         history.push("/")
       ).catch(async (res) => {
         const data = await res.json();
